d04647eb150c68ac5a76cca51ac0f89dadcfcef6
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2019-12-27 17:31:29 -0800

    js 19-12.27

--- Maximum Subarray.java
+++ Maximum Subarray.java
@@ -3,8 +3,8 @@ class Solution {
         int curmax = 0;
         int max = Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            curmax = Math.max(curmax+nums[i],nums[i]);
-            max = Math.max(max,curmax);
+            curmax = Math.max(curmax+nums[i],nums[i]);  //max local
+            max = Math.max(max,curmax);//global max
         }
         return max;
     }
