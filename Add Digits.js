41ecda47ddf606ccccec041b0c07e3128fa96b1f
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2019-12-30 02:41:42 -0800

    12-30

---
+++ Add Digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    if(num<10)
+        return num;
+    var sum = 0;
+    while(num>0){
+        var remain = num%10;
+        sum+=remain;
+        num = Math.floor(num/10);
+    }
+    var out = addDigits(sum);
+    return out;
+};
