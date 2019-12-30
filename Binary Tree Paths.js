fe4b86ce6b908eb5e283f5a751a9dd6e6392f7d4
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2019-12-30 02:25:34 -0800

    bfs/dfs/recursion

--- Binary Tree Paths.js
+++ Binary Tree Paths.js
@@ -9,19 +9,46 @@
  * @param {TreeNode} root
  * @return {string[]}
  */
+//var binaryTreePaths = function(root) {
+    //method1 recursion dfs
+//     var output = [];
+//     helper(root,"",output);
+//     return output;
+// };
+
+// var helper = function(root,path,output){
+//     if(!root)
+//         return;
+//     if(!root.left&&!root.right){
+//         output.push(path+root.val); 
+//     }
+//     helper(root.left,path+root.val+"->",output);
+//     helper(root.right,path+root.val+"->",output);
+// }
+
+//method2 dfs+stack
 var binaryTreePaths = function(root) {
-    //method1 recursion
-    var output = [];
-    helper(root,"",output);
+   if(!root)
+       return [];
+   var output = [];
+   var nodeStack = [];
+   var stringStack = [];
+   nodeStack.push(root);
+   stringStack.push("");
+   while(nodeStack.length!=0){
+        var curnode = nodeStack.pop();
+        var curstr = stringStack.pop();
+        if(curnode.left==null&&curnode.right==null){
+            output.push(curstr + curnode.val);
+        }
+        if(curnode.right!=null){
+           nodeStack.push(curnode.right);
+           stringStack.push(curstr + curnode.val + "->");
+        }
+       if(curnode.left!=null){
+           nodeStack.push(curnode.left);
+           stringStack.push(curstr + curnode.val + "->");           
+        }
+    }
     return output;
 };
-
-var helper = function(root,path,output){
-    if(!root)
-        return;
-    if(!root.left&&!root.right){
-        output.push(path+root.val); 
-    }
-    helper(root.left,path+root.val+"->",output);
-    helper(root.right,path+root.val+"->",output);
-}
