0a9100ede8ee91ac272e6f50ddbb0c098d38557a
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2019-12-30 02:47:28 -0800

    small change

--- Add Digits.js
+++ Add Digits.js
@@ -3,14 +3,26 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    if(num<10)
-        return num;
+    // if(num<10)
+    //     return num;
+    // var sum = 0;
+    // while(num>0){
+    //     var remain = num%10;
+    //     sum+=remain;
+    //     num = Math.floor(num/10);
+    // }
+    // var out = addDigits(sum);
+    // return out;
+    
     var sum = 0;
-    while(num>0){
+    while(num>9){
+       while(num>0){
         var remain = num%10;
         sum+=remain;
-        num = Math.floor(num/10);
+        num = Math.floor(num/10);           
+       }
+       num = sum;
+       sum = 0;
     }
-    var out = addDigits(sum);
-    return out;
+    return num;
 };
