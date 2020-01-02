7085ad8965175745d5bfdee232cdfb8a08ad7a76
zhuchenchen <zhuchenchen@std.uestc.edu.cn>
2020-01-02 15:44:08 -0800

    binary search

---
+++ First Bad Version.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        var low = 1;
+        var high = n;
+        var mid;
+        while(low<=high){
+          mid = Math.floor((low+high)/2);  
+          if(isBadVersion(mid)){
+            high=mid-1;
+          }else{
+              low=mid+1;
+          }            
+        }
+        return low;
+
+    };
+};
